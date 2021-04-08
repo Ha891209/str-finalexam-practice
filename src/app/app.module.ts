129cfbfce1e3710b6afad35f409c44cbbac42407
Aniko Kopis-Horvath <ha891209@gmail.com>
2021-04-08 20:40:15 +0200

    modified app.module, product.ts, README

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProductEditorComponent } from './product-editor/product-editor.component';
+import { HomeComponent } from './page/home/home.component';
+import { ProductEditorComponent } from './page/product-editor/product-editor.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ProductEditorComponent } from './product-editor/product-editor.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
